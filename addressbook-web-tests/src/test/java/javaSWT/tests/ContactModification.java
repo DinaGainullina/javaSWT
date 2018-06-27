2b21ceb7da2f7a41699ddd92c3f5c46ea70c3798
DinaGainullina <38789979+DinaGainullina@users.noreply.github.com>
2018-06-27 15:49:50 +0300

    добавлен тест по модификации карточки контакта

---
+++ addressbook-web-tests/src/test/java/javaSWT/tests/ContactModification.java
@@ -0,0 +1,25 @@
+package javaSWT.tests;
+
+import javaSWT.model.ContactData;
+import javaSWT.model.GroupData;
+import org.testng.annotations.Test;
+
+public class ContactModification extends  TestBase {
+
+  @Test
+  public void testGroupModification () {
+
+    app.getNavigationHelper().gotoHomePage();
+   //app.getGroupHelper().selectElement();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("TestD_LN", "TestD_FN", "TestD_address", "TestD_email1", "TestD_email2", "TestD_email3", "8843555456", "89645125452", "8545", "8546"));
+    app.getContactHelper().submitContactModification();
+
+
+
+
+
+  }
+
+
+}
