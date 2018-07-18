17cca71217359addea119c2a33dd8365735bad92
DinaGainullina <38789979+DinaGainullina@users.noreply.github.com>
2018-07-18 20:06:46 +0300

    доработка вычисления кол-ва решений уравнения

--- sandbox/src/main/java/ru/javaSWT/Equation.java
+++ sandbox/src/main/java/ru/javaSWT/Equation.java
@@ -16,24 +16,23 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
- /*   if (a == 0) {
+    if (a == 0) {
       if (b == 0) {
-        if (c = 0) {
+        if (c == 0) {
           n = -1;
         } else {
-          n = 1;
+          n = 0;
         }
+      } else {
+        n = 1;
       }
-    }
-      n = 1;
-    } else {*/
+    } else {
     if (d > 0) {
       n = 2;
-    } else {
-      if (d == 0) {
-        n = 1;
+    } else if (d == 0) {
+      n = 1;
       } else {
-        n = 0;
+      n = 0;
       }
     }
   }
