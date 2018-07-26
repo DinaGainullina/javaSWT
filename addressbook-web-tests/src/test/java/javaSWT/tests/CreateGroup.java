7244a2f0d50ad7c60d206446156c89ddc28d2c55
DinaGainullina <38789979+DinaGainullina@users.noreply.github.com>
2018-07-26 09:52:44 +0300

    Оптимизация заполнения форм: оставляем дефолтные значения в полях

--- addressbook-web-tests/src/test/java/javaSWT/tests/CreateGroup.java
+++ addressbook-web-tests/src/test/java/javaSWT/tests/CreateGroup.java
@@ -9,7 +9,8 @@ public class CreateGroup extends TestBase {
   public void testCreateGroup() {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("TestD1", "TestD2", "TestD3"));
+   // app.getGroupHelper().fillGroupForm(new GroupData("TestD1", "TestD2", "TestD3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("TestD1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }
